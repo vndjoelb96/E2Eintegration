f9bd28b0b56160ed9899dc69cdaf0698c242644e
VJ00618911 <VJ00618911@INBASDPC06701.TechMahindra.com>
2021-04-05 12:03:15 +0530

    added new test cases

---
+++ E2Eselenium-maven-github-jenkins/src/testcases/TC_004.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_004 {
+	
+	@Test
+	public void tc_001() 
+	{
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("hello");
+		driver.findElementById("pass").sendKeys("hello");
+		driver.quit();
+	}
+}
